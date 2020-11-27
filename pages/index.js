33291ac9e0a79228255eff60bd121bd48d514b7b
Aryan Ahire <aryanahire9086360780@gmail.com>
2020-11-26 23:54:26 -0500

    did some work

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,120 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Image from "next/image";
+
+var CONSTANTS = {
+  siteName: "Boring Games",
+  darkMode: true,
+};
 
 export default function Home() {
+  var title = "Boring-Games";
+
+  function toggleDarkMode() {
+    console.log(CONSTANTS.darkMode);
+    CONSTANTS.darkMode = !CONSTANTS.darkMode;
+  }
+
   return (
-    <div className="container">
+    <div class="container">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Home | {CONSTANTS.siteName}</title>
+        <link
+          rel="shortcut icon"
+          type="image/png"
+          href="/Boring-Games-Logo.png"
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css?family=Poppins"
+          rel="stylesheet"
+        ></link>
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+        <div
+          class="header"
+          style={{
+            "background-color": CONSTANTS.darkMode ? `black` : "white",
+          }}
+        >
+          <button onClick={toggleDarkMode}>Change Dark Mode</button>
+          <Image
+            src="/Boring-Games-Logo.png"
+            alt="Logo"
+            width={75}
+            height={75}
+            class="Logo"
+          />
+          <h1 class="space"> </h1>
+          <a href="game-page.html" class="nav">
+            <h3>Games</h3>
           </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="about.html" class="nav">
+            <h3>About</h3>
           </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="contact.html" class="nav">
+            <h3>Contact</h3>
           </a>
         </div>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
       <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+        .nav {
+          float: left;
+          font-family: "Poppins";
+          flex-direction: center;
         }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+        .header {
         }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
+        body {
           margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
+          padding: 0;
         }
       `}</style>
 
       <style jsx global>{`
-        html,
         body {
-          padding: 0;
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
+          padding: 0;
+          font-family: "Poppins";
+          scroll-behavior: smooth;
+          background-size: cover;
+          background-attachment: fixed;
+          overflow-x: hidden;
+          overflow-y: scroll;
         }
 
-        * {
-          box-sizing: border-box;
+        .logo {
+          width: 75px;
+          margin-top: 1%;
+          margin-left: 1%;
+          margin-bottom: 1%;
+        }
+
+        .header {
+          display: flex;
+          height: 100px;
+          width: 100%;
+          position: fixed;
+          transition: 0.3s;
+          z-index: 1000000;
+        }
+
+        .nav {
+          margin-left: 10%;
+          text-decoration: none;
+          margin-top: 0.5%;
+          font-size: 20px;
+          color: red;
+        }
+
+        .nav:hover {
+          color: black;
+        }
+
+        .space {
+          margin-left: 35%;
         }
       `}</style>
     </div>
-  )
+  );
 }
